d6a691e179abd35835b82c98f87111aa626af507
kerryyyang <kerryyang920910@gmail.com>
2019-10-03 21:53:38 +0800

    back testing

---
+++ backtesting.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ho_hmm import FilEst
+import pandas as pd
+import numpy as np
+import os
+
+
+class BackTCell(FilEst):
+    def __init__(self, dir_path, state, order, tns, dval, diff, freq, window,lamb, thresh):
+        self.dir_path=dir_path
+        self.state=state
+        self.order=order
+        self.tns=tns
+        self.dval=dval
+        self.diff=diff
+        self.freq=freq
+        self.window=window
+        self.lamb=lamb
+        self.thresh=thresh
+
+
+    def PredictionCell(self, factor_url, Kalman):
+        super(BackTCell, self).__init__(factor_url,self.state,self.order,self.tns,self.dval,self.diff,self.freq)
+        self.conFig()
+        return self.Algo(len(self.f)-tns, self.window, Kalman)
+
+    def Obj(self, args):
+        port_alpha=np.dot(args, 100*self.alpha.alpha.values)
+        port_factor=np.dot(args, np.dot(self.BETA, self.prediction.values))
+        h=np.dot(args, self.BETA)
+        port_factor_var=np.dot(np.dot(h, np.diag(self.fvar)),h.T)
+        h_epsilon=np.dot(args, self.epsilon)
+        port_epsilon=np.dot(np.dot(h_epsilon, self.eigen),h_epsilon.T)
+        rtn=port_alpha+port_factor
+        var=self.lamb*(port_factor_var+port_epsilon)
+
+        obj_fun=var-rtn
+        return   obj_fun
+
+
+    def cons(self, args):
+        return np.sum(args)
+    def cons_maker(self, i=0):
+        def cons_ineq(args):
+            return np.sign(self.scores[i])*np.dot(args,self.BETA[:,i])
+        return cons_ineq
+
+    def OptimizationCell(self, args):
+        from scipy.optimize import minimize
+
+        bnds=tuple([(-min(0.5, self.bmk[i]), 0.5) for i in range(len(self.bmk))])
+        cons=[{'type':'eq', 'fun':self.cons}]
+        for _, item in enumerate(self.scores):
+            if abs(item)>=self.thresh:
+                cons+=[{'type':'ineq', 'fun': self.cons_maker(i=_)}]
+            else:
+                cons+=[{'type':'eq', 'fun':self.cons_maker(i=_)}]
+        cons=tuple(cons)
+        sol=minimize(self.Obj, args, method='SLSQP', bounds=bnds, constraints=cons)
+        return sol
+
+
+
+    def Strgy(self, etf_url, Kalman):
+        etf=pd.read_excel(etf_url)
+        datelist=etf.Date.unique()
+
+        for i, date in enumerate(datelist):
+            t=str(date)
+            factor_url=self.dir_path+os.sep+'keyFactor/keyFactor_{}-{}-{}.xlsx'.format(t[:4],t[4:6],t[6:])
+            exp_url=self.dir_path+os.sep+'factorExposure/factorExposure_{}-{}-{}.xlsx'.format(t[:4], t[4:6],t[6:])
+            alpha_url=self.dir_path+os.sep+'alpha/alpha_{}-{}-{}.xlsx'.format(t[:4], t[4:6],t[6:])
+            epsilon_url=self.dir_path+os.sep+'epsilon/epsilon_{}-{}-{}.xlsx'.format(t[:4], t[4:6], t[6:])
+            eigen_url=self.dir_path+os.sep+'eigen/eigen_{}-{}-{}.xlsx'.format(t[:4], t[4:6], t[6:])
+
+            #load alpha
+            self.alpha=pd.read_excel(alpha_url)
+            self.alpha.columns=['Company','alpha']
+
+            #load exposure
+            self.beta=pd.read_excel(exp_url)
+            self.beta.columns=['Company']+list(self.beta.columns[1:])
+            self.BETA=self.beta[self.beta.columns[1:]].values
+
+            #load epsilon and eigenvalues
+            self.epsilon=pd.read_excel(epsilon_url)
+            self.epsilon=self.epsilon[self.epsilon.columns[1:]].values
+            self.eigen=pd.read_excel(eigen_url)
+            self.eigen=self.eigen[self.eigen.columns[1:]].values
+
+            #weight: take the whole universe into consideration
+            Wgt=pd.DataFrame(index=self.beta.Company)
+            etf_dy=etf[etf.Date==date][['Company','% Wgt']]
+            etf_dy.set_index('Company', inplace=True)
+            self.etf_dy=pd.merge(Wgt, etf_dy, left_index=True, right_index=True, how='outer').fillna(0).groupby('Company').sum()
+            self.bmk=self.etf_dy['% Wgt'].values
+
+
+            #call prediction function
+            self.prediction, self.scores=self.PredictionCell(factor_url, Kalman)
+
+            #call optimization function
+            Init_args=np.array([0.]*len(self.bmk))
+            self.opt=self.OptimizationCell(Init_args)
+            if self.opt.success:
+                if i==0:
+                    self.stgy_Wgt=pd.DataFrame()
+                    self.stgy_Wgt['Company']=self.etf_dy.index
+                    self.stgy_Wgt['Datetime']=date
+                    self.stgy_Wgt['% Wgt']=np.round(self.opt.x, 2)+self.etf_dy['% Wgt'].values
+
+                else:
+                    temp=pd.DataFrame()
+                    temp['Company']=self.etf_dy.index
+                    temp['Datetime']=date
+                    temp['% Wgt']=np.round(self.opt.x, 2)+self.etf_dy['% Wgt'].values
+                    self.stgy_Wgt=pd.concat([self.stgy_Wgt, temp], ignore_index=True)
+            else:
+                print(self.opt.message)
+                break
+            print("{} COMPLETED!".format(date))
+        self.stgy_Wgt.set_index('Datetime', inplace=True)
+        self.stgy_Wgt.to_excel('./data/pca/pca_stgy.xlsx')
+
+if __name__=="__main__":
+    dir_path="./data/pca"
+    etf_url='./data/pca/pca_etf.xlsx'
+    order=1
+    state=2
+    tns=63
+    dval=0.0001
+    diff=10
+    freq=10
+    window=252
+    lamb=1
+    thresh=0.5
+    bt=BackTCell(dir_path, state, order, tns, dval, diff, freq, window,lamb, thresh)
+    bt.Strgy(etf_url,False)
+
+
+
+
--- ho_hmm.py
+++ ho_hmm.py
@@ -3,26 +3,21 @@
 from config import Config
 import pandas as pd
 import numpy as np
-import os
+
 class FilEst(Config):
-    def __init__(self,state, order, tns, dval, diff,freq):
-        #Dataset
-        self.dir_path="./data/pca/keyFactor"
-        self.df=pd.read_excel(self.dir_path+os.sep+'keyFactor_2009-09-11.xlsx')
-        self.df.set_index('Date', inplace=True)
+    def __init__(self, url_factor, state, order, tns, dval, diff,freq):
+        self.df=pd.read_excel(url_factor)
+        self.df.set_index('Datetime', inplace=True)
         self.rtn=100*(self.df/self.df.shift(1)-1)
         self.pred_rtn=pd.DataFrame(columns=self.df.columns)
         self.pred_df=pd.DataFrame(columns=self.df.columns)
         self.diff=diff
-        self.f=100*(self.df.shift(-self.diff)/self.df-1).dropna()
+        self.f=100*(self.df/self.df.shift(self.diff)-1).dropna()
         super(FilEst, self).__init__(self.f, state, order, tns)
-        #Model Parameters
         self.D=[dval]*self.k
         self.freq=freq
-        #Algo results
         self.pf=pd.DataFrame(columns=self.f.columns)
-        self.p_path=[]
-        self.modp=[]
+
 
     def phik(self, s, v, v_1):
         from numpy.linalg import inv, det
@@ -56,10 +51,10 @@ class FilEst(Config):
                     prob=self.p.reshape(self.state, self.state).sum(axis=1)
                 else:
                     prob=self.p
-                f_tp=(prob.T*theta).A[0,:]*self.kf.loc[t-1]+(prob.T*mu).A[0,:]
-                sig_tp=(prob.T*theta).A[0,:]**2*self.Sigma+(prob.T*mu2).A[0,:]-(prob.T*mu).A[0,:]**2+ (prob.T*sigma2).A[0,:]
-                psi=sig_tp/(sig_tp+self.D)
-                self.kf.loc[t]=f_tp+psi*(self.f.iloc[t+self.tns-1].values-f_tp)
+                self.f_tp=(prob.T*theta).A[0,:]*self.kf.loc[t-1]+(prob.T*mu).A[0,:]
+                self.sig_tp=(prob.T*theta).A[0,:]**2*self.Sigma+(prob.T*mu2).A[0,:]-(prob.T*mu).A[0,:]**2+ (prob.T*sigma2).A[0,:]
+                psi=self.sig_tp/(self.sig_tp+self.D)
+                self.kf.loc[t]=self.f_tp+psi*(self.f.iloc[t+self.tns-1].values-self.f_tp)
                 self.Sigma=self.D*psi
                 self.D=((self.f.iloc[self.tns+self.loc-1:t+self.tns].values-self.kf.iloc[self.loc:t+1].values)**2).sum(axis=0)/t
         else:
@@ -81,7 +76,6 @@ class FilEst(Config):
             self.rho_T1=np.array([0.]*self.state**(self.order+1)*self.k).reshape(self.state**self.order,self.state,self.k)
             self.rho_T1_sq=np.array([0.]*self.state**(self.order+1)*self.k).reshape(self.state**self.order,self.state,self.k)
             self.rho_TT1=np.array([0.]*self.state**(self.order+1)*self.k).reshape(self.state**self.order,self.state,self.k)
-
         else:
             if Kalman:
                 v=self.kf.iloc[t].values
@@ -93,8 +87,14 @@ class FilEst(Config):
                 E=np.asmatrix(np.diag([self.phik(i,v,v_1) for i in range(self.state) for j in range(self.state)]))
             else:
                 E=np.asmatrix(np.diag([self.phik(i,v,v_1) for i in range(self.state)]))
-            op=(E*self.p).A[:,0]/np.sum(E*self.p)
-            m=E/np.sum(E*self.p)
+            #in case of E=diag([0,0,...,0])
+            if np.sum(E*self.p)>0:
+                op=(E*self.p).A[:,0]/np.sum(E*self.p)
+                m=E/np.sum(E*self.p)
+            else:
+                op=np.array([0.]*self.state**self.order)
+                m=np.asmatrix(np.diag([1.]*self.state**self.order))
+
             for _r in range(self.state):
                 if self.order==2:
                     for _s in range(self.state):
@@ -137,11 +137,18 @@ class FilEst(Config):
                         #i->j
                         i=_s*self.state+_t
                         j=_r*self.state+_s
-                        self.args[j*self.state**2+i]=J[_r,_s,_t]/O2[_s,_t]
+                        if O2[_s, _t]==0:
+                            self.args[j*self.state**2+i]= 0
+                        else:
+                            self.args[j*self.state**2+i]=J[_r,_s,_t]/O2[_s,_t]
         else:
             for _r in range(self.state):
                 for _s in range(self.state):
-                    self.args[_r*self.state+_s]=J[_r,_s]/O[0,_s]
+                    if O[0,_s]==0:
+                        self.args[_r*self.state+_s]=0
+                    else:
+                        self.args[_r*self.state+_s]=J[_r,_s]/O[0,_s]
+
         T=self.rho_T.sum(axis=0)
         T1=self.rho_T1.sum(axis=0)
         T_sq=self.rho_T_sq.sum(axis=0)
@@ -151,18 +158,17 @@ class FilEst(Config):
         #args_old=self.args.copy()
         k=[]; m=[];
         for i in range(self.state):
-            k.append(self.args[ix+i*self.k:ix+(i+1)*self.k])
-            m.append(self.args[ix+(i+self.state)*self.k:ix+(i+1+self.state)*self.k])
-            a=T_sq[i]+k[i]**2*T1_sq[i]+m[i]**2*O[0,i]-2*k[i]*TT1[i]-2*m[i]*T[i]+2*m[i]*k[i]*T1[i]
-            self.args[ix+i*self.k:ix+(i+1)*self.k]=(TT1[i]-m[i]*T1[i])/T1_sq[i]
-            self.args[ix+(i+self.state)*self.k:ix+(i+1+self.state)*self.k]=(T[i]-k[i]*T1[i])/O[0,i]
-            self.args[ix+(2*self.state+i)*self.k:ix+(2*self.state+i+1)*self.k]= np.array(list(map(lambda x: max(x, 0.0001),a/O[0,i])))
-        print("O:------")
-        print(O)
-        if self.order==2:
-            print("O2:-----")
-            print(O2)
-        print("============UPDATE COMPLETED!==============")
+            if O[0,i]==0:
+                k.append(self.args[ix+i*self.k:ix+(i+1)*self.k])
+                m.append(self.args[ix+(i+self.state)*self.k:ix+(i+1+self.state)*self.k])
+            else:
+                k.append(self.args[ix+i*self.k:ix+(i+1)*self.k])
+                m.append(self.args[ix+(i+self.state)*self.k:ix+(i+1+self.state)*self.k])
+                a=T_sq[i]+k[i]**2*T1_sq[i]+m[i]**2*O[0,i]-2*k[i]*TT1[i]-2*m[i]*T[i]+2*m[i]*k[i]*T1[i]
+                self.args[ix+i*self.k:ix+(i+1)*self.k]=(TT1[i]-m[i]*T1[i])/T1_sq[i]
+                self.args[ix+(i+self.state)*self.k:ix+(i+1+self.state)*self.k]=(T[i]-k[i]*T1[i])/O[0,i]
+                self.args[ix+(2*self.state+i)*self.k:ix+(2*self.state+i+1)*self.k]= np.array(list(map(lambda x: max(x, 0.0001),a/O[0,i])))
+
 
     def Algo(self, period, window, Kalman=False):
         self.kalman(0, False, Kalman)
@@ -171,7 +177,6 @@ class FilEst(Config):
             if t%self.freq==0:
                 self.kalman(t, False, Kalman)
                 self.hmm(t, Kalman)
-                print("The {}_th Update Begin!".format(t//self.freq))
                 self.EM()
                 self.hmm(0, Kalman)  #restart HMM filter
                 _=max(1, t-window+1)
@@ -184,9 +189,6 @@ class FilEst(Config):
             else:
                 self.kalman(t,False, Kalman)
                 self.hmm(t, Kalman)
-
-            self.p_path.append(self.p.A[:,0])
-            self.modp.append(list(self.args))
             if self.order==2:
                 prob=self.p.reshape(self.state, self.state).sum(axis=1)
             else:
@@ -201,36 +203,24 @@ class FilEst(Config):
             self.pred_df.loc[t-1]=(1+0.01*self.pf.loc[t-1])*self.df.iloc[t+self.tns]
             self.pred_rtn.loc[t-1]=100*(self.pred_df.loc[t-1]/self.df.iloc[t+self.tns+self.diff-1]-1)
 
-        tf=self.f.index[self.tns+1:self.tns+period]
-        td=self.df.index[self.tns+1+self.diff: self.tns+1+self.diff+period]
-        self.pf.index=tf
-        self.pred_df.index=td
-        self.pred_rtn.index=td
-        #retrive daily-return prediction from the period-return time series
-        self.pred_df.to_excel('./output/KalmanEM/pred_df.xlsx'.format(self.order, self.state))
-        self.pred_rtn.to_excel('./output/KalmanEM/pred_rtn.xlsx'.format(self.order, self.state))
-        self.r_df=self.df[(self.df.index>=td[0]) &(self.df.index<=td[-1])]
-        self.r_df.to_excel('./output/KalmanEM/r_df.xlsx'.format(self.order, self.state))
-        self.r_rtn=self.rtn[(self.rtn.index>=td[0]) &(self.rtn.index<=td[-1])]
-        self.r_rtn.to_excel('./output/KalmanEM/r_rtn.xlsx'.format(self.order, self.state))
-        #period-return prediction and the corresponding model parameters
-        self.pf.to_excel('./output/KalmanEM/pf.xlsx'.format(self.order, self.state))
-        self.r=self.f[(self.f.index>=tf[0]) &(self.f.index<=tf[-1])]
-        self.r.to_excel('./output/KalmanEM/r.xlsx'.format(self.order, self.state))
-        pd.DataFrame(self.p_path, index=tf).to_excel('./output/KalmanEM/p.xlsx'.format(self.order,self.state))
-        pd.DataFrame(self.modp, index=tf).to_excel('./output/KalmanEM/modp.xlsx'.format(self.order,self.state))
-
-if __name__=="__main__":
-    order=1 #you can also test higher order HMM here
-    state=2
-    tns=63
-    diff=10
-    freq=10
-    dval=0.0001
-    ins=FilEst(state, order, tns,dval, diff,freq)
-    ins.conFig()
-    ins.Algo(period=len(ins.f)-tns, window=252, Kalman=True)
-
+        '''
+        Last-step prediction
+        '''
+        t=period
+        Sig=self.args[ix+2*self.state*self.k:ix+3*self.state*self.k].reshape(self.state, self.k)
+        if Kalman:
+            self.kalman(t, False, Kalman)
+            pf_pred=(prob.T*theta).A[0]*self.kf.iloc[-1].values+(prob.T*mu).A[0]
+        else:
+            pf_pred=(prob.T*theta).A[0]*self.f.iloc[t+self.tns-1].values+(prob.T*mu).A[0]
+        df_pred=(1+0.01*pf_pred)*self.df.iloc[t+self.tns]
+        rtn_pred=100*(df_pred/self.df.iloc[t+self.tns+self.diff-1]-1)
+        if Kalman:
+            self.fvar=self.sig_tp
+            return rtn_pred, rtn_pred/np.sqrt(self.fvar)
+        else:
+            self.fvar=(prob.T*Sig).A[0]
+            return rtn_pred, rtn_pred/np.sqrt(self.fvar)
 
 
 
